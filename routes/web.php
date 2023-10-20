243a72723043fb51da63ee15ea332fba0f802614
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2023-10-21 01:01:16 +0700

    fitur logout

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Auth::routes(
 	]
 );
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['prefix' => 'dashboard', 'middleware' => ['auth']], function () {
 
